8c89f0b5f920d4b5d9cfc0fdbae3023c4d87fa3d
FilCoster <filcoster122@gmail.com>
2017-07-05 15:34:42 +0700

    Add funk(2)

---
+++ src/funk.h
@@ -0,0 +1,10 @@
+#ifndef FUNK
+#define FUNK
+
+float Discriment(float a,float b,float c);
+
+int Proverka(float D, float a, float b, float c);
+
+float Znachenie (float D, float a, float b, float c, int i);
+
+#endif
